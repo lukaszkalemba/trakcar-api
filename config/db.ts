1e5390d4a50136bcd575b38c9ae7846521337e71
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-09-15 19:01:39 +0200

    Configure project

---
+++ config/db.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import colors from 'colors';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './config.env' });
+
+const connectDB = async (): Promise<void> => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI as string, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(colors.cyan.bold(`MongoDB Connected: ${conn.connection.host}`));
+  } catch (err) {
+    console.log(colors.red(`Error: ${err.message}`));
+    process.exit(1);
+  }
+};
+
+export default connectDB;
