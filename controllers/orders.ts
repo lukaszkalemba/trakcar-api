eaacaade8cc2558a1f0801dde786750fe139e7bb
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-09-16 21:33:27 +0200

    Create editing an order route

--- controllers/orders.ts
+++ controllers/orders.ts
@@ -51,6 +51,60 @@ export const addOrder = async (req: Request, res: Response): Promise<any> => {
   }
 };
 
+// @desc    Edit an order
+// @route   PUT /api/v1/orders/:id
+// @access  Public
+export const editOrder = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const order = await Order.findById(req.params.id);
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        error: 'No order found',
+      });
+    }
+
+    const {
+      positionId,
+      orderDate,
+      startTime,
+      endTime,
+      orderName,
+      carBrand,
+      carModel,
+      principalName,
+      cost,
+      orderColor,
+      description,
+    } = req.body;
+
+    order.positionId = positionId;
+    order.orderDate = orderDate;
+    order.startTime = startTime;
+    order.endTime = endTime;
+    order.orderName = orderName;
+    order.carBrand = carBrand;
+    order.carModel = carModel;
+    order.principalName = principalName;
+    order.cost = cost;
+    order.orderColor = orderColor;
+    order.description = description;
+
+    await order.save();
+
+    return res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error',
+    });
+  }
+};
+
 // @desc    Delete an order
 // @route   DELETE /api/v1/orders/:id
 // @access  Public
--- routes/api/orders.ts
+++ routes/api/orders.ts
@@ -1,10 +1,15 @@
 import express from 'express';
-import { getOrders, addOrder, deleteOrder } from 'controllers/orders';
+import {
+  getOrders,
+  addOrder,
+  editOrder,
+  deleteOrder,
+} from 'controllers/orders';
 
 const router = express.Router();
 
 router.route('/').get(getOrders).post(addOrder);
 
-router.route('/:id').delete(deleteOrder);
+router.route('/:id').put(editOrder).delete(deleteOrder);
 
 export default router;
