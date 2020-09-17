155ba5866c942382907f7dac2be2af045b7634d6
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-09-17 17:58:32 +0200

    Create adding new position route

--- controllers/positions.ts
+++ controllers/positions.ts
@@ -20,3 +20,36 @@ export const getPositions = async (_: Request, res: Response): Promise<any> => {
     });
   }
 };
+
+// @desc    Add new position
+// @route   POST /api/v1/positions
+// @access  Public
+export const addPosition = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    const position = await Position.create(req.body);
+
+    return res.status(201).json({
+      success: true,
+      data: position,
+    });
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      const messages = Object.values(err.errors).map(
+        ({ message }: any): string => message
+      );
+
+      return res.status(400).json({
+        success: false,
+        error: messages,
+      });
+    }
+
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error',
+    });
+  }
+};
