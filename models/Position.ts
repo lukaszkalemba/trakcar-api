0ba3fe86c2568d06b68adf89d8e0c648afa12f3c
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-09-17 17:38:41 +0200

    Create position data model using mongoose

---
+++ models/Position.ts
@@ -0,0 +1,25 @@
+import { Document, Schema, model } from 'mongoose';
+
+const PositionSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: [true, 'Enter a position name'],
+  },
+  startTime: {
+    type: String,
+    required: [true, 'Enter the time the work starts on the position'],
+  },
+  endTime: {
+    type: String,
+    required: [true, 'Enter the time the work ends on the position'],
+  },
+});
+
+interface IPositionSchema extends Document {
+  name: string;
+  startTime: string;
+  endTime: string;
+}
+
+export default model<IPositionSchema>('Position', PositionSchema);
