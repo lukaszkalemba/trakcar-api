a622e2f9f587f18597cbdf76d69cba6c4c2a380b
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-09-17 18:20:20 +0200

    Create editing a position route

--- controllers/positions.ts
+++ controllers/positions.ts
@@ -54,6 +54,43 @@ export const addPosition = async (
   }
 };
 
+// @desc    Edit a position
+// @route   PUT /api/v1/positions/:id
+// @access  Public
+export const editPosition = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    const position = await Position.findById(req.params.id);
+
+    if (!position) {
+      return res.status(404).json({
+        success: false,
+        error: 'No position found',
+      });
+    }
+
+    const { name, startTime, endTime } = req.body;
+
+    position.name = name;
+    position.startTime = startTime;
+    position.endTime = endTime;
+
+    await position.save();
+
+    return res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error',
+    });
+  }
+};
+
 // @desc    Delete a position
 // @route   DELETE /api/v1/positions
 // @access  Public
--- routes/api/positions.ts
+++ routes/api/positions.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import {
   getPositions,
   addPosition,
+  editPosition,
   deletePosition,
 } from 'controllers/positions';
 
@@ -9,6 +10,6 @@ const router = express.Router();
 
 router.route('/').get(getPositions).post(addPosition);
 
-router.route('/:id').delete(deletePosition);
+router.route('/:id').put(editPosition).delete(deletePosition);
 
 export default router;
