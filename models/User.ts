5774ea1f0bb0040b867dc1d61f9345150a1b2e37
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-09-21 12:59:01 +0200

    Create user model

---
+++ models/User.ts
@@ -0,0 +1,39 @@
+import { Document, Schema, model } from 'mongoose';
+import isEmail from 'validator/lib/isEmail';
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Enter a name'],
+    minlength: [6, 'Name must be at least 6 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Enter an email'],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Enter a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Enter a password'],
+    minlength: [6, 'Password must be at least 6 characters'],
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+interface IUserSchema extends Document {
+  name: string;
+  email: string;
+  password: string;
+  avatar?: string;
+  date?: Date;
+}
+
+export default model<IUserSchema>('User', UserSchema);
