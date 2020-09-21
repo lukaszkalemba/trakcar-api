04bd61623d71a16146a9e644d9a7819b6dfab47b
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-09-21 17:39:40 +0200

    Separate signup validation to middleware

--- routes/api/auth.ts
+++ routes/api/auth.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import validateSignup from 'middleware/validateSignup';
 import { signUpUser } from 'controllers/auth';
 
 const router = express.Router();
 
-router.route('/signup').post(signUpUser);
+router.route('/signup').post(validateSignup, signUpUser);
 
 export default router;
