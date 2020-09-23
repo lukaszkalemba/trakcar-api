6a3158b25210a65d23bab678a322808c5a52ba94
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-09-23 22:16:17 +0200

    Create middleware which validates user

---
+++ middleware/validateUser.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+const validateUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Response | void => {
+  const token = req.header('x-auth-token');
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      error: 'No token, authorization denied',
+    });
+  }
+
+  try {
+    jwt.verify(token, <string>process.env.JWT_SECRET);
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      error: 'Token is not valid',
+    });
+  }
+};
+
+export default validateUser;
