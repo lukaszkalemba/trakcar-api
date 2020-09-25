de38011ce4f4a1c4ce1a4e2e060b68e4bc80e88c
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-09-25 22:15:22 +0200

    Create deleting an organization route

--- routes/api/organizations.ts
+++ routes/api/organizations.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import auth from 'middleware/auth';
 import {
   organizations_create_organization,
+  organizations_delete_organization,
   organizations_assign_member,
 } from 'controllers/organizations';
 
 const router = express.Router();
 
 router.post('/', auth, organizations_create_organization);
+
+router.delete('/:id', auth, organizations_delete_organization);
 
 router.post('/assign-member', auth, organizations_assign_member);
 
