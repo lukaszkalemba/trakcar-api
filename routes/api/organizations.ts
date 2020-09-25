0a0f454f6bdc2ec4609748fff2a44350cef5e73e
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-09-25 20:17:16 +0200

    Create adding new member to the organization route

--- routes/api/organizations.ts
+++ routes/api/organizations.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import auth from 'middleware/auth';
-import { organizations_create_organization } from 'controllers/organizations';
+import {
+  organizations_create_organization,
+  organizations_assign_member,
+} from 'controllers/organizations';
 
 const router = express.Router();
 
 router.post('/', auth, organizations_create_organization);
+
+router.post('/assign-member', auth, organizations_assign_member);
 
 export default router;
